04374884101aed9eee6bf0bd477696822c5a2e0f
DELL_INSPIRON\shuhei <shuhei.kawazoe123@gmail.com>
2024-05-31 18:34:52 +0900

    [fix] レイアウトを修正、三目並べゲームを個別ページに移動

--- src/Routing.jsx
+++ src/Routing.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Link, BrowserRouter, Routes } from 'rea
 import { Home } from "./components/Home";
 import { List } from "./components/wishlist/list";
 import { Form } from "./components/wishlist/form";
+import { Game } from "./components/wishlist/game";
 
 function AppRouter() {
   return (
@@ -19,13 +20,17 @@ function AppRouter() {
             <li>
               <Link to="/form/">投稿</Link>
             </li>
+            <li>
+              <Link to="/game/">三目並べゲーム</Link>
+            </li>
           </ul>
         </nav>
-        <Link to="/user/">User</Link>
+        {/* <Link to="/user/">User</Link> */}
         <Routes>
           <Route path="/" exact element={Home()} />
           <Route path="/list/" element={List()} />
           <Route path="/form/" element={Form()} />
+          <Route path="/game/" element={Game()} />
         </Routes>
       </div>
     </BrowserRouter>
