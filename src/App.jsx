9db75903b9096121962c7331cd8cf49f7a9d12d9
DELL_INSPIRON\shuhei <shuhei.kawazoe123@gmail.com>
2025-02-05 22:55:46 +0900

    CognitoをHTTPS通信でテスト

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,97 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Link, BrowserRouter, Routes, useNavigate } from 'react-router-dom';
 import Routing from './Routing.jsx'
+import { useAuth } from "react-oidc-context";
+import Login from './components/Login';
 
-export default function App() {
+function App() {
+  const auth = useAuth();
+  const [urlToken, setUrlToken] = useState(null);
+
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const token = params.get('code');
+    if (token) {
+      setUrlToken(token);
+    }
+  }, []);
+
+  const signOutRedirect = () => {
+    const clientId = "3u1317g4pjb9g72ubasbdpnpa9";
+    const logoutUri = "http://localhost:5173";
+    const cognitoDomain = "https://wishlist.auth.ap-southeast-2.amazoncognito.com";
+    window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
+  };
+
+  switch (auth.activeNavigator) {
+    case "signinSilent":
+        return <div>Signing you in...</div>;
+    case "signoutRedirect":
+        return <div>Signing you out...</div>;
+  }
+
+  if (auth.isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (auth.error) {
+    return <div>Encountering error... {auth.error.message}</div>;
+  }
+
+  if (auth.isAuthenticated) {
+    return (
+      <div>
+        <pre> Hello: {auth.user?.profile.email} </pre>
+        <pre> ID Token: {auth.user?.id_token} </pre>
+        <pre> Access Token: {auth.user?.access_token} </pre>
+        <pre> Refresh Token: {auth.user?.refresh_token} </pre>
+
+        <button onClick={() => auth.removeUser()}>Sign out</button>
+      </div>
+    );
+  }
+
   return (
-    <div className='app'>
-      <BrowserRouter>
-        < Routing />
-      </BrowserRouter>
+    <div>
+      {urlToken && (
+        <div>
+          <pre>URL Token: {urlToken}</pre>
+          <pre> Hello: {auth.user?.profile.email} </pre>
+        <pre> ID Token: {auth.user?.id_token} </pre>
+        <pre> Access Token: {auth.user?.access_token} </pre>
+        <pre> Refresh Token: {auth.user?.refresh_token} </pre>
+        </div>
+      )}
+      <button onClick={() => auth.signinRedirect()}>Sign in</button>
+      <button onClick={() => signOutRedirect()}>Sign out</button>
     </div>
   );
 }
+
+
+  
+export default App;
+
+// export default function App() {
+//   const auth = useAuth();
+
+//     // if (auth.isLoading) {
+//     //     return <div>読み込み中...</div>;
+//     // }
+
+//     // if (auth.error) {
+//     //     return <div>エラーが発生しました: {auth.error.message}</div>;
+//     // }
+
+//     // if (!auth.isAuthenticated) {
+//     //     return <Login />;
+//     // }
+//   return (
+//     <div className='app'>
+//       <BrowserRouter>
+//         < Routing />
+//       </BrowserRouter>
+//     </div>
+//   );
+// }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,29 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { AuthProvider } from "react-oidc-context";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const cognitoAuthConfig = {
+  authority: "https://cognito-idp.ap-southeast-2.amazonaws.com/ap-southeast-2_GJNA9YPP7",
+  client_id: "3u1317g4pjb9g72ubasbdpnpa9",
+  redirect_uri: "http://localhost:5173", // あなたのアプリケーションのURL
+  response_type: "code",
+  scope: "email openid profile", // 必要なスコープを指定
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// wrap the application with AuthProvider
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <AuthProvider {...cognitoAuthConfig}>
+      <App />
+    </AuthProvider>
+  </React.StrictMode>
+);
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
